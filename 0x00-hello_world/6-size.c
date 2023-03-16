544364bc6cc2ca150f70fc66676b947c59f307d7
MEHDI-BILI <midibili6@gmail.com>
2023-03-16 13:42:01 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	fprintf(stderr, "Size of a char: %lu byte(s)\n", sizeof(char));
-	fprintf(stderr, "size of an int: %lu byte(s)\n", sizeof(int));
-	fprintf(stderr, "size of a long int: %lu byte(s)\n", sizeof(long int));
-	fprintf(stderr, "size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	fprintf(stderr, "Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("size of an int: %lu byte(s)\n", sizeof(int));
+	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
