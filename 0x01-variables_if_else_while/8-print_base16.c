21fd67aa11333aef6a90c2d303c2ab5f4f409acc
MEHDI-BILI <midibili6@gmail.com>
2023-03-17 16:04:27 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - digits print base 16
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j < 'g' ; j++)
+	putchar('\n');
+	return (0);
+}
