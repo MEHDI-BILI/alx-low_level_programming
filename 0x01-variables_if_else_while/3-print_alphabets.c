418379f3bad68aa3bb3c3e6c42f5c5e853689ed5
MEHDI-BILI <midibili6@gmail.com>
2023-03-17 13:51:57 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet printed in lowercase
+ * Return: 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+	putchar('\n');
+	return (0);
+}
