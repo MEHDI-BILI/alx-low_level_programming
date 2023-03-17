38b8d9f3c54f828cf501f7c828bd11c1c5c0837e
MEHDI-BILI <midibili6@gmail.com>
2023-03-17 15:51:10 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet printed in lowercase z to a
+ * Return: 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i <= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
