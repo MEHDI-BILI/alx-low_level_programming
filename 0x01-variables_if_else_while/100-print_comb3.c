3b43545dac1d8ec3eeb2c8fa7eb5c3cac759ceca
MEHDI-BILI <midibili6@gmail.com>
2023-03-17 18:31:50 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - print digits base 10 combinations task 100
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 1 ; j < 10 ; j++)
+		{
+			if (i < j)
+			{
+				putchar (i + '0');
+				putchar (j + '0');
+				if (i != 8 && j != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
