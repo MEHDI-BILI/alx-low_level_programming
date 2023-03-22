5a94e24f131cafa30d0d49fcf6cbd3459643da97
MEHDI-BILI <midibili6@gmail.com>
2023-03-21 17:11:54 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_sign - prints signs
+ *
+ * @n: Parameter
+ * Return: 0 or 1 or -1 depending on result
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}
