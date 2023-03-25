235bf6ca14ddbaca75dfee073c6aa2f327f5001c
MEHDI-BILI <midibili6@gmail.com>
2023-03-24 17:04:34 -0700

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - if digit or not
+ *@c: character to determine
+ *return 1 for digit else 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
