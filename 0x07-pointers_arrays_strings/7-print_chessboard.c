37ed67aa8411a68209ad8a913b260259f0acca91
MEHDI-BILI <midibili6@gmail.com>
2023-04-03 19:41:17 -0700

    6/7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: the array ?
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, n;
+
+	for (i = 0; i < 0; i++)
+	{
+		for (n = 0; n < 8; n++)
+			_putchar(a[i][n]);
+		_putchar('\n');
+	}
+}
