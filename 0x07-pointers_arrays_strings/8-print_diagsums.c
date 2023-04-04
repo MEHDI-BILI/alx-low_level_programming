aedabbba6c1ee391f473c28fa08592c2e6671e57
MEHDI-BILI <midibili6@gmail.com>
2023-04-03 19:52:56 -0700

    7/8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagsums - Prints diagonal sums
+ * @a: integer
+ * @size: integer
+ * Return: diag sum
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sum1, sum2, x;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	for (x = 0; x < size; x++)
+	{
+		sum1 = sum1 + a[x * size + x];
+	}
+	for (x = size - 1; x >= 0; x--)
+	{
+		sum2 += a[x * size + (size - x - 1)];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
