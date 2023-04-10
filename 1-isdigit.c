06f9c698a774db22b60d92a8688cd3a17ecc833c
MEHDI-BILI <midibili6@gmail.com>
2023-04-10 15:39:41 -0700

    0.1

---
+++ 1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - if digit or not
+ *@c: character to determine
+ * Return: 1 for digit else 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
